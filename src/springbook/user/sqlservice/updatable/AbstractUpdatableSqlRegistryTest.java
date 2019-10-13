a437d8ca5b7d9770b1b04fba7bb1f45d04ab0da9
ukh <able.kiho.um@gmail.com>
2019-10-13 22:20:53 +0900

    UpdatableSqlRegistry 테스트 코드의 재사용

---
+++ src/springbook/user/sqlservice/updatable/AbstractUpdatableSqlRegistryTest.java
@@ -0,0 +1,70 @@
+package springbook.user.sqlservice.updatable;
+
+import org.junit.Before;
+import org.junit.Test;
+import springbook.user.sqlservice.SqlNotFoundException;
+import springbook.user.sqlservice.SqlUpdateFailureException;
+import springbook.user.sqlservice.UpdatableSqlRegistry;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.assertThat;
+
+public abstract class AbstractUpdatableSqlRegistryTest {
+    UpdatableSqlRegistry sqlRegistry;
+    
+    @Before
+    public void setUp() {
+        sqlRegistry = createUpdatableSqlRegistry();
+        sqlRegistry.registerSql("KEY1","SQL1");
+        sqlRegistry.registerSql("KEY2","SQL2");
+        sqlRegistry.registerSql("KEY3","SQL3");
+    }
+
+    protected abstract UpdatableSqlRegistry createUpdatableSqlRegistry();
+
+    protected void checkFind(String expected1, String expected2, String expected3) {
+
+    }
+
+    @Test
+    public void find() {
+        checkFindResult("SQL1", "SQL2", "SQL3");
+    }
+
+    private void checkFindResult(String sql1, String sql2, String sql3)  {
+
+        assertThat(sqlRegistry.findSql("KEY1"), is(sql1));
+        assertThat(sqlRegistry.findSql("KEY2"), is(sql2));
+        assertThat(sqlRegistry.findSql("KEY3"), is(sql3));
+
+    }
+
+    @Test(expected = SqlNotFoundException.class)
+    public void unknownKey() {
+        sqlRegistry.findSql("SQL4444");
+    }
+
+    @Test
+    public void updateSingle() {
+        sqlRegistry.updateSql("KEY2", "Modified2");
+        checkFindResult("SQL1", "Modified2","SQL3");
+    }
+
+    @Test
+    public void updateMulti() {
+        Map<String, String> sqlmap = new HashMap<>();
+        sqlmap.put("KEY1", "Modified1");
+        sqlmap.put("KEY3", "Modified3");
+
+        sqlRegistry.updateSql(sqlmap);
+        checkFindResult("Modified1", "SQL2","Modified3");
+    }
+
+    @Test(expected = SqlUpdateFailureException.class)
+    public void updateWithNotExistingKey() {
+        sqlRegistry.updateSql("SQL33434", "Modifed2");
+    }
+}
--- src/springbook/user/sqlservice/updatable/ConcurrentHashMapSqlRegistryTest.java
+++ src/springbook/user/sqlservice/updatable/ConcurrentHashMapSqlRegistryTest.java
@@ -12,54 +12,10 @@ import java.util.Map;
 import static org.hamcrest.core.Is.is;
 import static org.junit.Assert.assertThat;
 
-public class ConcurrentHashMapSqlRegistryTest {
-    UpdatableSqlRegistry sqlRegistry;
+public class ConcurrentHashMapSqlRegistryTest extends AbstractUpdatableSqlRegistryTest{
 
-
-    @Before
-    public void setUp() {
-        sqlRegistry = new ConcurrentHashMapSqlRegistry();
-        sqlRegistry.registerSql("KEY1","SQL1");
-        sqlRegistry.registerSql("KEY2","SQL2");
-        sqlRegistry.registerSql("KEY3","SQL3");
-    }
-
-    @Test
-    public void find() {
-        checkFindResult("SQL1", "SQL2", "SQL3");
-    }
-
-    private void checkFindResult(String sql1, String sql2, String sql3)  {
-
-        assertThat(sqlRegistry.findSql("KEY1"), is(sql1));
-        assertThat(sqlRegistry.findSql("KEY2"), is(sql2));
-        assertThat(sqlRegistry.findSql("KEY3"), is(sql3));
-
-    }
-
-    @Test(expected = SqlNotFoundException.class)
-    public void unknownKey() {
-        sqlRegistry.findSql("SQL4444");
-    }
-
-    @Test
-    public void updateSingle() {
-        sqlRegistry.updateSql("KEY2", "Modified2");
-        checkFindResult("SQL1", "Modified2","SQL3");
-    }
-
-    @Test
-    public void updateMulti() {
-        Map<String, String> sqlmap = new HashMap<>();
-        sqlmap.put("KEY1", "Modified1");
-        sqlmap.put("KEY3", "Modified3");
-
-        sqlRegistry.updateSql(sqlmap);
-        checkFindResult("Modified1", "SQL2","Modified3");
-    }
-
-    @Test(expected = SqlUpdateFailureException.class)
-    public void updateWithNotExistingKey() {
-        sqlRegistry.updateSql("SQL33434", "Modifed2");
+    @Override
+    protected UpdatableSqlRegistry createUpdatableSqlRegistry() {
+        return new ConcurrentHashMapSqlRegistry();
     }
 }
